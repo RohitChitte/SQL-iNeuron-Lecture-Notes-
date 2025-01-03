b05b33d4a45099365db007296c96d4307f9d3040
Rohit Sukhdev Chitte <chitterohit2112@gmail.com>
2025-01-03 15:33:09 +0530

    latest

--- SQL Codebasics/Lect 9.7 Stored Proceduer for Monthly Gross Sales report.sql
+++ SQL Codebasics/Lect 9.7 Stored Proceduer for Monthly Gross Sales report.sql
@@ -14,14 +14,14 @@ on fsm.product_code=fgp.product_code and get_fiscal_year(fsm.date)=fgp.fiscal_ye
  # Creating Stored Proceduer from above query
  /*
  CREATE PROCEDURE get_monthly_gross_sales_for_customer (
-	c_code INT
+	in_customer_code TEXT
 )
 BEGIN
 	select date,sum(gross_price*sold_quantity) as total_sales from 
 	fact_sales_monthly as fsm
 	join fact_gross_price as fgp
 	on fsm.product_code=fgp.product_code and get_fiscal_year(fsm.date)=fgp.fiscal_year
-	 where customer_code = c_code
+	 where find_in_set(fsm.customer_code, in_customer_code)>0
      group by date;
 END
 */
